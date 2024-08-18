63ac8289fa249cc4288a04cb206e9e15dff5e512
Samuels5 <samimule20@gmail.com>
2024-08-19 00:27:08 +0300

    adding

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,12 @@ export default function Home() {
   const { status } = useSession();
   return (
     <div className="pt-44 flex text-4xl">
+      {cookies.get("access-token") == undefined &&
+      status == "unauthenticated" ? (<>
         <Link href='/login'>Autenticate first</Link>
-        <Link href='/app' className="ml-24">continue to joblist</Link>
+        <Link href='/app' className="ml-24">continue to joblist</Link></>):(
+          <Link href='/app' className="ml-24">continue to joblist</Link>
+        )}
     </div>
   );
 }
