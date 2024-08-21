3c94dc39f3e65a57333d6c7a9f5e2bbcddd8dbf8
Samuels5 <samimule20@gmail.com>
2024-08-21 08:08:05 +0300

    adding

--- app/app/Cards.tsx
+++ app/app/Cards.tsx
@@ -4,9 +4,12 @@ import Image from "next/image";
 import { useEffect } from "react";
 import { useState } from "react";
 import Book from "./componet/Book";
+import { useCookies } from "next-client-cookies";
+import { useSession } from "next-auth/react";
 
 const Cards = ({ job, ind, c, st, handler }: { job: any; ind: number; c : boolean, st:string, handler:any}) => {
-
+  const cookies = useCookies();
+  const { status, data: session } = useSession();
   // console.log('s',job) 
   return (
     <div className="p-6 rounded-3xl border my-9 border-gray-400 divide-black">
@@ -19,7 +22,9 @@ const Cards = ({ job, ind, c, st, handler }: { job: any; ind: number; c : boolea
             <h1 className="font-mono font-semibold text-2xl text-indigo-950">
               {job.title}
             </h1>
-            <Book id={job.id} c={c} st={st} handler={handler} />
+                  {cookies.get("access-token") == undefined &&
+      status == "unauthenticated" ? (<div> </div>):(
+            <Book id={job.id} c={c} st={st} handler={handler} />)}
           </div>
           <h4 className="font-extralight text-gray-400">
             {job.orgName} .{" "}
