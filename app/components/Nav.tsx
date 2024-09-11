2f9134d7df56719a1523661be051b5ccd061e5e1
Samuels5 <samimule20@gmail.com>
2024-09-11 16:52:42 +0300

    finilizing

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,6 +4,7 @@ import { useCookies } from "next-client-cookies";
 import { useSession } from "next-auth/react";
 import Nav1 from "./Nav1";
 import Nav2 from "./Nav2";
+
 const Nav = () => {
     const cookies = useCookies();
     const { status, data: session } = useSession();
